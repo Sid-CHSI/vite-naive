dfe9328e37e8934b7421e45129e06b378ff16685
Sid Huang <sid.huang@ch-si.com.tw>
2022-11-23 17:39:41 +0800

    tsx props

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,7 +1,18 @@
 import { defineComponent } from "vue";
 
-export default defineComponent({
-  setup() {
-    return () => <div>Hellow TSX SID</div>;
+const helloWorld = defineComponent({
+  props: {
+    msg: String,
+  },
+  render() {
+    return (
+      <div className="flex flex-col">
+        <h1 className="green text-2xl">Hellow TSX</h1>
+        <div>{this.msg} SID</div>
+        <div className="nest text-red-500">RED</div>
+      </div>
+    );
   },
 });
+
+export default helloWorld;
